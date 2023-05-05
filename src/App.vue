4b5ea26ed713dc1ae1a0aae01fbc8a25889fe7ca
hyot <allsdfgh88@gmail.com>
2023-05-05 17:05:10 +0900

    52강 Todo 페이지 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">Kossie Coder</router-link>
+      <router-link class="navbar-brand" :to="{name: 'Home'}">Kossie Coder</router-link>
 
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/todos">Todos</router-link>
+          <router-link class="nav-link active" aria-current="page" :to="{name: 'Todos'}">Todos</router-link>
         </li>
       </ul>
     </div>
   </nav>
-  <router-view />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <script>
